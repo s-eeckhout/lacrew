ca6dd45e5c251fd438805e155308f35206bd6f2b
wzywzywzy <654483577wzy@gmail.com>
2024-03-23 09:50:12 +0100

    add recipes

--- backend/app/api/routes.py
+++ backend/app/api/routes.py
@@ -2,9 +2,11 @@ from flask import Blueprint
 from flask_restful import Api
 from ..resources.hello_world import HelloWorld
 from ..resources.welcome import Welcome
+from ..resources.recipe import Recipe
 
 api_bp = Blueprint('api', __name__)
 api = Api(api_bp)
 
 api.add_resource(HelloWorld, '/helloworld')
 api.add_resource(Welcome, '/')
+api.add_resource(Recipe, '/recipes', '/recipes/<int:id>')

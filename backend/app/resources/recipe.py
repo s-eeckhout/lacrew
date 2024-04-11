2d843db3b7e34bc77b3e21743607dc231013c660
wzywzywzy <654483577wzy@gmail.com>
2024-04-11 10:14:24 +0200

    add functions of recipes in backend

--- backend/app/resources/recipe.py
+++ backend/app/resources/recipe.py
@@ -2,25 +2,36 @@ import json
 from flask_restful import Resource
 from flask import jsonify
 
-class Recipe(Resource):
-    def get(self, id=None):
-        name = 'recipe_' + str(id)
-        #get recipes from recipes folder by id(eg. id=1 get Pasta recipe)
-        read_file = open('/Users/wangziyi/Desktop/KTH study/lacrew/lacrew/backend/app/recipes/recipes.json', 'r')
-        data = json.load(read_file)
-        for recipe_key, recipe_value in data.items():
-            if recipe_key == name:
-                return jsonify(recipe_value)
-        return jsonify({'message': 'Recipe not found!'})
+path = 'backend/app/recipes/recipes.json'
+class RecipeList(Resource):
+    def get(self):
+        with open(path, 'r') as read_file:
+            data = json.load(read_file)
+        return data
 
-    def post(self):
-        data = {'message': 'Recipe added!'}
-        return jsonify(data)
-    def put(self):
-        data = {'message': 'Recipe updated!'}
-        return jsonify(data)
-    def delete(self):
-        data = {'message': 'Recipe deleted!'}
-        return jsonify(data)
+class RecipeById(Resource):
+    def get(self, recipe_id):
+        with open(path, 'r') as read_file:
+            data = json.load(read_file)
+        recipe_key = f"recipe_{recipe_id}"
+        recipe = data.get(recipe_key)
+        if recipe:
+            return recipe
+        return {'message': 'Recipe not found!'}, 404
+
+class RecipeByIngredient(Resource):
+    def get(self, ingredient):
+        with open(path, 'r') as read_file:
+            data = json.load(read_file)
+        matching_recipes = []
+        for recipe in data.values():
+            print(recipe)
+            print(666)
+            if ingredient.lower() in [ing.lower() for ing in recipe.get('ingredients', [])]:
+                matching_recipes.append(recipe)
+        if matching_recipes:
+            return matching_recipes
+        return {'message': f'No recipes found containing the ingredient {ingredient}.'}, 404
+
 # Path: backend/app/resources/recipe.py
     
